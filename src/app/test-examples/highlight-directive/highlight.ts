cd6a664823b9babba9bee114f6ad05ee114e7004
tibor.czifra <tibor.czifra@dynata.com>
2026-08-26 15:01:19 +0200

    modify directive

--- src/app/test-examples/highlight-directive/highlight.ts
+++ src/app/test-examples/highlight-directive/highlight.ts
@@ -1,17 +1,12 @@
-import { Directive, ElementRef, effect, inject, input } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
+  host: {
+    '[style.background-color]': 'appHighlight() || defaultColor()',
+  },
 })
 export class Highlight {
   readonly appHighlight = input('');
   readonly defaultColor = input('yellow');
-  readonly #elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
-
-  constructor() {
-    effect(() => {
-      this.#elementRef.nativeElement.style.backgroundColor =
-        this.appHighlight() || this.defaultColor();
-    });
-  }
 }
